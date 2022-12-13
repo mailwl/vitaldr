f9ec6b1c27a7c3d6f6b31cf78a3ac8c21e89ddbf
Владимир Русских <vrusskih@niisokb.ru>
2022-12-13 14:36:48 +0300

    add imports nids

--- vitaldr.cpp
+++ vitaldr.cpp
@@ -1,6 +1,8 @@
 
 #include "vitaldr.h"
 
+#undef _CONSOLE
+
 #ifdef _CONSOLE
 
 #include <stdio.h>
@@ -14,6 +16,18 @@ typedef std::string qstring;
 #define SETPROC_ALL 1
 #define SETPROC_FATAL 2
 #define ACCEPT_FIRST 4
+#define FILEREG_PATCHABLE 1
+#define NAME_CODE ".code"
+#define CLASS_CODE "CODE"
+#define NAME_DATA ".data"
+#define CLASS_DATA "DATA"
+
+#define qsnprintf snprintf 
+#define qchar char
+
+typedef uint64_t qoff64_t;
+typedef uint32_t ea_t;
+typedef void* segment_t;
 
 static uint32_t qlsize(linput_t* li) {
     fseek(li, 0, SEEK_END);
@@ -38,14 +52,53 @@ static void set_processor_type(const char*, uint32_t) {
     // nothing to do
 }
 
+int file2base(linput_t* li, qoff64_t pos, ea_t ea1, ea_t ea2, int patchable) {
+    return 1;
+}
+
+bool add_segm(ea_t para, ea_t start, ea_t end, const char* name, const char* sclass, int flags = 0) {
+    return true;
+}
+
+void loader_failure() {
+}
+
+segment_t* getseg(ea_t) {
+    return nullptr;
+}
+
+void set_segm_addressing(segment_t* s, int) {
+}
+
+void create_filename_cmt() {
+}
+
+void add_entry(ea_t, ea_t, const char*, bool makecode, int flags = 0) {
+
+}
+
+int mem2base(const void* memptr, ea_t ea1, ea_t ea2, qoff64_t fpos) {
+    return 1;
+}
+
+bool append_cmt(ea_t ea, const char* str, bool rptble) {
+    return true;
+}
+
+
+
 #else
 #include <idaldr.h>
+#include <segment.hpp>
 #pragma comment(lib, "ida")
 #endif
 
 static self_header_t self_header;
 static app_info_t app_info;
 static elf_header_t elf_header_fake, elf_header;
+static std::vector<elf_phdr_t> program_headers;
+static std::vector<segment_info_t> seg_infos;
+static SegmentInfosForReloc segment_reloc_info;
 
 void load_header(linput_t* li) {
     qlseek(li, 0);
@@ -55,26 +108,115 @@ void load_header(linput_t* li) {
     qlseek(li, static_cast<uint32_t>(self_header.elf_offset));
     qlread(li, &elf_header_fake, sizeof elf_header_t);
     qlseek(li, static_cast<uint32_t>(self_header.header_len));
-}
-
-int idaapi accept_file(qstring *fileformatname, qstring *processor, linput_t *li, const char *filename) {
-    load_header(li);
-
-    if((self_header.magic == 0x00454353) && (self_header.version == 3)) {
-        // TODO: check if prx
-        *fileformatname = "Sony Playstation Vita eboot.bin";
-        *processor = "arm";
-        return 1 | ACCEPT_FIRST;
-    }
-    return 0;
-}
-
-void idaapi load_file(linput_t *li, uint16_t neflags, const char *fileformatname) {
-    load_header(li);
-    qlseek(li, static_cast<uint32_t>(self_header.header_len));
-
     qlread(li, &elf_header, sizeof elf_header_t);
 
+}
+extern "C"
+int idaapi accept_file(qstring *fileformatname, qstring *processor, linput_t *li, const char *filename) {
+    
+    load_header(li);
+
+    bool test = (self_header.magic == 0x00454353) && (self_header.version == 3);
+    if (!test) return 0;
+
+    test = elf_header.e_machine == EM_ARM;
+
+    if (!test) return 0;
+    
+
+    *fileformatname = "Sony Playstation Vita eboot.bin";
+    *processor = "arm";
+
+    return 1 | ACCEPT_FIRST;
+}
+extern "C"
+void idaapi load_file(linput_t *li, uint16_t neflags, const char *fileformatname) {
+
+    load_header(li);
+
+    for (int i = 0; i < elf_header.e_phnum; ++i) {
+        segment_info_t si;
+        qlseek(li, self_header.section_info_offset + i * sizeof(segment_info_t));
+        qlread(li, &si, sizeof segment_info_t);
+        seg_infos.push_back(si);
+
+        qlseek(li, (elf_header.e_phoff + self_header.header_len) + i * elf_header.e_phentsize);
+        elf_phdr_t ph;
+        qlread(li, &ph, elf_header.e_phentsize);
+        program_headers.push_back(ph);
+
+        if (ph.p_type == PT_LOAD) {
+            
+            std::vector<char> buf(ph.p_filesz);
+            qlseek(li, ph.p_offset + self_header.header_len);
+            qlread(li, buf.data(), ph.p_filesz);
+
+            
+            if (si.compression == 2) {
+                // TODO: decompession
+            }
+            else {
+                mem2base(buf.data(), ph.p_vaddr, ph.p_vaddr + ph.p_filesz, ph.p_offset + self_header.header_len);
+                segment_reloc_info[i] = { std::move(buf), ph.p_vaddr, ph.p_memsz };
+            }
+
+            bool is_code = (PF_X & ph.p_flags) == PF_X;
+
+            if (!add_segm(0, ph.p_vaddr, ph.p_vaddr + ph.p_memsz, is_code ? NAME_CODE : NAME_DATA, is_code ? CLASS_CODE : CLASS_DATA)) {
+                loader_failure();
+            }
+            segment_t* s = getseg(ph.p_vaddr);
+            set_segm_addressing(s, 1);
+        }
+        else if (ph.p_type == PT_SCE_RELA) {
+            // TODO: relocate?
+        }
+        
+    }
+    const uint32_t module_info_offset = elf_header.e_entry & 0x3fffffff;
+    const uint32_t module_info_segment_index = static_cast<uint32_t>(elf_header.e_entry >> 30);
+    const char* module_info_segment_bytes = (const char*)(segment_reloc_info[module_info_segment_index].addr.data());
+    const sce_module_info_t* const module_info = reinterpret_cast<const sce_module_info_t*>(module_info_segment_bytes + module_info_offset);
+
+    const sce_module_exports_t* exports = reinterpret_cast<const sce_module_exports_t*>(module_info_segment_bytes + module_info->export_top);
+    const sce_module_imports_t* imports = reinterpret_cast<const sce_module_imports_t*>(module_info_segment_bytes + module_info->import_top);
+    
+    uint32_t exports_count = (module_info->export_end - module_info->export_top) / sizeof(sce_module_exports_t);
+    uint32_t imports_count = (module_info->import_end - module_info->import_top) / sizeof(sce_module_imports_t);
+
+    for (int i = 0; i < exports_count; ++i) {
+        // TODO: set adresses to IDA
+        
+        const uint32_t* func_nids = reinterpret_cast<const uint32_t*>(module_info_segment_bytes + (exports->nid_table - program_headers[module_info_segment_index].p_vaddr));
+        const uint32_t* func_entry = reinterpret_cast<const uint32_t*>(module_info_segment_bytes + (exports->entry_table - program_headers[module_info_segment_index].p_vaddr));
+        for (uint32_t i = 0; i < exports->num_syms_funcs; ++i) {
+            msg("  export nid: 0x%x, entry: 0x%x\n", func_nids[i], func_entry[i]);
+        }
+        exports++;
+    }
+
+    for (uint32_t i = 0; i < imports_count; ++i) {
+        const char* lib_name = module_info_segment_bytes + (imports->library_name - program_headers[module_info_segment_index].p_vaddr);
+
+        msg("imports from library: %s\n", lib_name);
+
+        const uint32_t* func_nids = reinterpret_cast<const uint32_t*>(module_info_segment_bytes + (imports->func_nid_table - program_headers[module_info_segment_index].p_vaddr));
+        const uint32_t* func_entry = reinterpret_cast<const uint32_t*>(module_info_segment_bytes + (imports->func_entry_table - program_headers[module_info_segment_index].p_vaddr));
+        for (uint32_t j = 0; j < imports->num_syms_funcs; ++j) {
+            char buffer[0x100];
+            qsnprintf(buffer, 0x100, "%s_0x%x", lib_name, func_nids[j]);
+            msg("  import nid: 0x%x, entry: 0x%x\n", func_nids[j], func_entry[j]);
+            //set_cmt(func_entry[j], buffer, false);
+            set_name(func_entry[j], buffer);
+        }
+
+        imports++;
+    }
+
+    uint32_t entry = program_headers[module_info_segment_index].p_vaddr + module_info->module_start;
+    add_entry(entry, entry, "_start", true);
+
+    create_filename_cmt();
 }
 
 
@@ -111,6 +253,7 @@ bool APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     return true;
 }
 
+extern "C"
 loader_t LDSC{
     IDP_INTERFACE_VERSION,
     0,
--- vitaldr.h
+++ vitaldr.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
+#include <map>
+#include <vector>
 
 typedef uint32_t Elf32_Word;
 typedef uint32_t Elf32_Off;
@@ -30,6 +32,13 @@ typedef struct {
 } self_header_t;
 
 typedef struct {
+    uint64_t offset;
+    uint64_t length;
+    uint64_t compression; // 1 = uncompressed, 2 = compressed
+    uint64_t encryption; // 1 = encrypted, 2 = plain
+} segment_info_t;
+
+typedef struct {
     uint64_t authid;                /* auth id */
     uint32_t vendor_id;             /* vendor id */
     uint32_t self_type;             /* app type 0x0D - , */
@@ -38,6 +47,19 @@ typedef struct {
 } app_info_t;
 
 #define EI_NIDENT 16
+#define EM_ARM (0x28)
+
+enum phdr_flags : uint32_t {
+    PF_X = 0x1,               // Execute
+    PF_W = 0x2,               // Write
+    PF_W_X = 0x3,             // Execute+Write
+    PF_R = 0x4,               // Read
+    PF_R_X = 0x5,             // Read, execute
+    PF_R_W = 0x6,             // Read, write
+    PF_R_W_X = 0x7,           // Read, write, execute
+    PF_MASKOS = 0x0ff00000,   // Unspecified
+    PF_MASKPROC = 0xf0000000, // Unspecified
+};
 
 typedef struct {
     unsigned char   e_ident[EI_NIDENT];
@@ -47,7 +69,7 @@ typedef struct {
     Elf32_Addr      e_entry;
     Elf32_Off       e_phoff;
     Elf32_Off       e_shoff;
-    Elf32_Word      e_flags;
+    phdr_flags      e_flags;
     Elf32_Half      e_ehsize;
     Elf32_Half      e_phentsize;
     Elf32_Half      e_phnum;
@@ -55,6 +77,13 @@ typedef struct {
     Elf32_Half      e_shnum;
     Elf32_Half      e_shstrndx;
 } elf_header_t;
+
+#define PT_NULL (0x0U) // Unused entry - skip
+#define PT_LOAD (0x1U) // Loadable segment
+#define PT_SCE_RELA (0x60000000U) // Relocations
+#define PT_SCE_COMMENT (0x6FFFFF00U) // Compiler signature?
+#define PT_SCE_VERSION (0x6FFFFF01U) // SDK signature?
+#define PT_ARM_EXIDX (0x70000001U)
 
 typedef struct {
     Elf32_Word p_type;
@@ -93,5 +122,67 @@ typedef union {
         Elf32_Word r_word2;
         Elf32_Word r_word3;
     } r_raw_entry;
-} SCE_Rel;
+} sce_rel;
 
+typedef uint32_t Address;
+struct SegmentInfoForReloc {
+    std::vector<char> addr; // segment address in guest memory
+    Address p_vaddr; // segment virtual address in guest memory
+    uint64_t size; // segment memory size
+};
+using SegmentInfosForReloc = std::map<uint16_t, SegmentInfoForReloc>;
+
+typedef struct sce_module_exports_t {
+    uint16_t size;				/* Size of this struct, set to 0x20 */
+    uint16_t version;			/* 0x1 for normal export, 0x0 for main module export */
+    uint16_t flags;				/* 0x1 for normal export, 0x8000 for main module export */
+    uint16_t num_syms_funcs;		/* Number of function exports */
+    uint32_t num_syms_vars;			/* Number of variable exports */
+    uint32_t num_syms_tls_vars;     /* Number of TLS variable exports */
+    uint32_t library_nid;			/* NID of this library */
+    uint32_t library_name;	/* Pointer to name of this library */
+    uint32_t nid_table;		/* Pointer to array of 32-bit NIDs to export */
+    uint32_t entry_table;	/* Pointer to array of data pointers for each NID */
+};
+
+typedef struct sce_module_imports_t {
+    uint16_t size;				/* Size of this struct, set to 0x34 */
+    uint16_t version;			/* Set to 0x1 */
+    uint16_t flags;				/* Set to 0x0 */
+    uint16_t num_syms_funcs;		/* Number of function imports */
+    uint16_t num_syms_vars;			/* Number of variable imports */
+    uint16_t num_syms_tls_vars;     /* Number of TLS variable imports */
+
+    uint32_t reserved1;
+    uint32_t library_nid;			/* NID of library to import */
+    uint32_t library_name;	/* Pointer to name of imported library, for debugging */
+    uint32_t reserved2;
+    uint32_t func_nid_table;	/* Pointer to array of function NIDs to import */
+    uint32_t func_entry_table;/* Pointer to array of stub functions to fill */
+    uint32_t var_nid_table;	/* Pointer to array of variable NIDs to import */
+    uint32_t var_entry_table;	/* Pointer to array of data pointers to write to */
+    uint32_t tls_var_nid_table; /* Pointer to array of TLS variable NIDs to import */
+    uint32_t tls_var_entry_table; /* Pointer to array of data pointers to write to */
+};
+
+typedef struct sce_module_info_t { // size is 0x5C-bytes
+    uint16_t attributes;
+    uint16_t version;			/* Set to 0x0101 */
+    char name[27];				/* Name of the library */
+    uint8_t type;				/* 0x0 for executable, 0x6 for PRX */
+    uint32_t gp_value;
+    uint32_t export_top;			/* Offset to start of export table */
+    uint32_t export_end;			/* Offset to end of export table */
+    uint32_t import_top;			/* Offset to start of import table */
+    uint32_t import_end;			/* Offset to end of import table */
+    uint32_t module_nid;			/* NID of this module */
+    uint32_t tls_start;
+    uint32_t tls_filesz;
+    uint32_t tls_memsz;
+    uint32_t module_start;	/* Offset to function to run when library is started, 0 to disable */
+    uint32_t module_stop;	/* Offset to function to run when library is exiting, 0 to disable */
+    uint32_t exidx_top;	/* Offset to start of ARM EXIDX (optional) */
+    uint32_t exidx_end;	/* Offset to end of ARM EXIDX (optional) */
+    uint32_t extab_top;	/* Offset to start of ARM EXTAB (optional) */
+    uint32_t extab_end;	/* Offset to end of ARM EXTAB (optional */
+};
